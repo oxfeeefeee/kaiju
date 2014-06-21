6c48c94a5415a7d6e2c6e4028e8c12a1497f10c5
oxfeeefeee <number5.ios@gmail.com>
2014-06-21 23:28:54 +0800

    Added missing "main" package
    .gitignore is edited so that the binary file gets ignored but main.go
    is not

---
+++ main/main.go
@@ -0,0 +1,55 @@
+// This project is offically started today on April 2nd, 2014. BTC price is now at 450USD. 
+package main
+
+import (
+    "time"
+    "runtime"
+    "math/rand"
+    "github.com/oxfeeefeee/kaiju"
+    "github.com/oxfeeefeee/kaiju/profiling"
+    "github.com/oxfeeefeee/kaiju/kio"
+    "github.com/oxfeeefeee/kaiju/brain"
+    "github.com/oxfeeefeee/kaiju/blockchain"
+)
+
+func mainCleanUp(){
+    kaiju.MainLogger().Printf("Cleaning up...")
+    err := blockchain.CloseFiles()
+    if err != nil {
+        kaiju.MainLogger().Printf("Failed to close files: %s", err.Error())
+    }
+}
+
+func mainFunc() {
+    runtime.GOMAXPROCS(runtime.NumCPU())
+    
+    profiling.RunProfiler()
+
+    rand.Seed(time.Now().UTC().UnixNano())
+
+    err := kaiju.ReadJsonConfigFile()
+    if err != nil {
+        kaiju.MainLogger().Printf("Failed to ready config file: %s", err.Error())
+        return;
+    }
+
+    err = blockchain.InitFiles()
+    if err != nil {
+        kaiju.MainLogger().Printf("Failed to init files: %s", err.Error())
+        return;
+    }
+
+    kaiju.MainLogger().Printf("starting kio...")
+    <- kio.Start(10)
+    kaiju.MainLogger().Printf("kio initialized.")
+    brain.Start()
+    kaiju.MainLogger().Printf("brain started.")
+
+    // Don't quit
+    c := make(chan struct{})
+    _ = <- c
+}
+
+func main() {
+    mainFunc()
+}
---
+++ main/main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "testing"
+    )
+
+// So that we dont have to type "go build" every time
+func TestMain(t *testing.T) {
+    mainFunc()
+}
