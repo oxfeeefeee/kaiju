df0d926c9790090e015454e708f0b4b8a8f41716
oxfeeefeee <number5.ios@gmail.com>
2014-06-22 20:34:13 +0800

    Bitcoin script: HashToSign
    Completed the tx.HashToSign function which returns the input of ECDSA
    for
    signing/verifing signature.

--- catma/tx.go
+++ catma/tx.go
@@ -6,6 +6,13 @@ import (
     "github.com/oxfeeefeee/kaiju/klib"
 )
 
+const (
+    SIGHASH_ALL             = 1
+    SIGHASH_NONE            = 2
+    SIGHASH_SINGLE          = 3
+    SIGHASH_ANYONECANPAY    = 0x80 // This is not a type but a flag
+)
+
 type TxOut struct {
     // Transaction Value
     Value           int64
@@ -58,3 +65,85 @@ func (t *Tx) Bytes() []byte {
     binary.Write(p, binary.LittleEndian, t.LockTime)
     return p.Bytes()
 }
+
+// Returns the string to sign for an input of a TX, invalid index causes panic
+func (t *Tx) HashToSign(subScript []byte, ii int, hashType uint32) *klib.Hash256 {
+    if ii >= len(t.TxIns) {
+        panic("Tx.StringToSign invalid index")
+    }
+    anyoneCanPay := (hashType & SIGHASH_ANYONECANPAY) != 0
+    htype := hashType & 0x1f
+    p := new(bytes.Buffer)
+
+    // STEP0: version 
+    binary.Write(p, binary.LittleEndian, t.Version)
+
+    // STEP1: inputs 
+    if (anyoneCanPay) != 0 {
+        // If SIGHASH_ANYONECANPAY is set, only current input is written, 
+        // and subScipt is used as SigScript 
+        p.Write(klib.VarUint(1).Bytes())                                // inputs count
+        binary.Write(p, binary.LittleEndian, t.TxIns[ii].PreviousOutput)// PreviousOutput
+        p.Write(((*klib.VarString)(&subScript)).Bytes())                // subScript
+        binary.Write(p, binary.LittleEndian, txin.Sequence)             // Sequence
+    } else {
+        // Else write all the inputs with modifications
+        p.Write(klib.VarUint(len(t.TxIns)).Bytes())
+        binary.Write(p, binary.LittleEndian, txin.PreviousOutput)
+        for i, txin := range t.TxIns {
+            if i == ii { // If this is current input, write subScript
+                p.Write(((*klib.VarString)(&subScript)).Bytes())
+            } else { // Else write an empty VarString
+                p.Write((klib.VarString{0}).Bytes())
+            }
+            sequence := txin.Sequence
+            if i != ii && (htype == SIGHASH_NONE || htype == SIGHASH_SINGLE) {
+                // If not current input, and of type SIGHASH_NONE || SIGHASH_SINGLE,
+                // set sequence to 0
+                sequence = 0
+            }
+            binary.Write(p, binary.LittleEndian, sequence)
+        } 
+    }
+
+    // STEP3: outputs
+    switch htype {
+    case SIGHASH_NONE:
+        p.Write((klib.VarString{0}).Bytes())
+    case SIGHASH_SINGLE:
+        if ii >= len(t.TxOuts) {
+            // This is actually allowed due to a bug in Satoshi client, should do this:
+            // panic("Tx.StringToSign invalid index with type SIGHASH_SINGLE")
+            return new(klib.Hash256).SetUint64(1)
+        }
+        p.Write(klib.VarUint(ii + 1).Bytes()) // output count
+        for i:=0; i < ii; i++ { // All outputs except the last one are written as blank
+            binary.Write(p, binary.LittleEndian, int64(-1)) // value
+            p.Write((klib.VarString{0}).Bytes()) // script
+        }
+        txout := t.TxOuts[ii]
+        binary.Write(p, binary.LittleEndian, txout.Value)
+        p.Write(((*klib.VarString)(&txout.PKScript)).Bytes())
+    case SIGHASH_ALL:
+        p.Write(klib.VarUint(len(t.TxOuts)).Bytes())
+        for _, txout := range t.TxOuts {
+            binary.Write(p, binary.LittleEndian, txout.Value)
+            p.Write(((*klib.VarString)(&txout.PKScript)).Bytes())
+        }
+    default:
+        panic("Tx.StringToSign invalid hash type")
+    }
+
+    // STEP4: LockTime and HashType
+    binary.Write(p, binary.LittleEndian, t.LockTime)
+    binary.Write(p, binary.LittleEndian, hashType)
+    return klib.Sha256Sha256(p.Bytes())
+}
+
+
+
+
+
+
+
+
--- klib/hash256.go
+++ klib/hash256.go
@@ -10,18 +10,31 @@ import (
 // A 256 bit hash, e.g. result of sha256
 type Hash256 [32]byte
 
+// Little endian
+func (h *Hash256) SetUint64(v uint64) *Hash256 {
+    h[0] = byte(v)
+    h[1] = byte(v >> 8)
+    h[2] = byte(v >> 16)
+    h[3] = byte(v >> 24)
+    h[4] = byte(v >> 32)
+    h[5] = byte(v >> 40)
+    h[6] = byte(v >> 48)
+    h[7] = byte(v >> 56)
+    return h
+}
+
 // MSB first string
-func (h *Hash256) SetString(s string) error {
+func (h *Hash256) SetString(s string) (*Hash256, error) {
     data, err := hex.DecodeString(s)
     if err != nil {
-        return err
+        return nil, err
     } else if (len(data) != 32) {
-        return errors.New("Hash256.SetString invalid length.") 
+        return nil, errors.New("Hash256.SetString invalid length.") 
     }
     for i := 0; i<32; i++ {
         h[i] = data[31-i]
     }
-    return nil
+    return h, nil
 }
 
 func (h *Hash256) String() string {
